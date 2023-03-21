923952f28e5ed0a57d4c5be84c87388251d2d0da
김양재 <kyj95112@naver.com>
2023-03-21 17:19:18 +0900

    feat : Backend 환경설정

---
+++ backend/src/main/java/com/ssafy/tlens/TlensApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TlensApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/com/ssafy/tlens/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.ssafy.tlens.config;
+
+import com.ssafy.tlens.common.auth.JwtAuthenticationFilter;
+import com.ssafy.tlens.common.exception.handler.JwtExceptionFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * 인증(authentication) 와 인가(authorization) 처리를 위한 스프링 시큐리티 설정 정의.
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    private final WebMvcConfig webMvcConfig;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final JwtExceptionFilter jwtExceptionFilter;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.cors().configurationSource(webMvcConfig.corsConfigurationSource())
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
