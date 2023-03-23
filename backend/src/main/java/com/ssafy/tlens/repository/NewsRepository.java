906b73b78734e0cbd499d0cb1dc1245494a97dd8
강주형 <kakao_panda@naver.com>
2023-03-23 17:55:58 +0900

    HOTFIX: NewsRepository 수정

--- backend/src/main/java/com/ssafy/tlens/repository/NewsRepository.java
+++ backend/src/main/java/com/ssafy/tlens/repository/NewsRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News,String> {
-    List<News> findNewsAllByOrderByNewsNo();
+    // List<News> findNewsAllByOrderByNewsNo();
 }
