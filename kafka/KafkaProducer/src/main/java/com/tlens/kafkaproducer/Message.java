ee41a62afde725be33fac033c405a23dc57021bd
김양재 <kyj95112@naver.com>
2023-03-27 17:57:08 +0900

    feat : kafka consumer 기능 추가

---
+++ kafka/KafkaProducer/src/main/java/com/tlens/kafkaproducer/Message.java
@@ -0,0 +1,11 @@
+package com.tlens.kafkaproducer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+    private String Name;
+    private String message;
+}
