85fc1674dead2e0e23b413c819b03bb6d26eda5f
김양재 <kyj95112@naver.com>
2023-03-30 11:37:51 +0900

    feat : kafka consumer 기능 추가

--- kafka/KafkaConsumer/src/main/java/com/tlens/kafkaconsumer/KafkaConsumer.java
+++ kafka/KafkaConsumer/src/main/java/com/tlens/kafkaconsumer/KafkaConsumer.java
@@ -1,61 +1,96 @@
 package com.tlens.kafkaconsumer;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
+import com.tlens.kafkaconsumer.util.KafkaConstants;
+import org.apache.hadoop.shaded.org.eclipse.jetty.websocket.common.frames.DataFrame;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.OutputMode;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import scala.Tuple2;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 @Component
-@RequiredArgsConstructor
-public class KafkaConsumer implements CommandLineRunner {
+public class KafkaConsumer implements ApplicationRunner {
 
-    private static final String TOPIC_NAME = "tlens";
+//    @Override
+//    public void run(ApplicationArguments args) throws TimeoutException, StreamingQueryException {
+//        SparkConf conf = new SparkConf()
+//                .setAppName(KafkaConstants.TOPIC_NAME)
+//                .setMaster("local[2]");
+//
+//        Dataset<Row> df = SparkSession.builder()
+//                .config(conf)
+//                .getOrCreate()
+//                .readStream()
+//                .format("kafka")
+//                .option("kafka.bootstrap.servers", "j8c206.p.ssafy.io:9092")
+//                .option("subscribe", KafkaConstants.TOPIC_NAME)
+//
+//                .option("startingOffsets", "earliest")
+//                .option("maxOffsetsPerTrigger", 10)
+//                .option("stopGracefullyOnShutdown", true)
+//
+//                .option("group.id", "consumer")
+//                .load();
+//
+//        df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+//                .writeStream()
+//                .format("console")
+//                .outputMode(OutputMode.Append())
+//                .start()
+//                .awaitTermination();
+//    }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(ApplicationArguments args) {
         SparkConf conf = new SparkConf()
-                .setAppName(TOPIC_NAME)
-                .setMaster("spark://cluster.p.ssafy.io:39735")
-                .set("spark.driver.allowMultipleContexts", "true");
-        JavaStreamingContext jsc = new JavaStreamingContext(conf, new Duration(100));
+                .setAppName(KafkaConstants.TOPIC_NAME)
+                .setMaster("local[2]");
+//                .setMaster("spark://cluster.p.ssafy.io:43463");
 
-        Map<String, Object> kafkaParams = new HashMap<String, Object>();
-        kafkaParams.put("bootstrap.servers", "j8c206.p.ssafy.io:9092");
-        kafkaParams.put("group.id", "consumer-tlens");
-        kafkaParams.put("auto.offset.reset", "earliest");
-        kafkaParams.put("enable.auto.commit", false);
+        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(5));
 
-        Collection<String> topics = Arrays.asList("tlens");
+        Map<String, Object> kafkaParams = Map.of(
+                "bootstrap.servers", "j8c206.p.ssafy.io:9092",
+                "group.id", "consumer-tlens-1",
+                "key.deserializer", StringDeserializer.class.getName(),
+                "value.deserializer", StringDeserializer.class.getName(),
+                "key.serializer", StringSerializer.class.getName(),
+                "value.serializer", StringSerializer.class.getName(),
+                "auto.offset.reset", "earliest",
+                "enable.auto.commit", false
+        );
 
-        JavaInputDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(
-                jsc, LocationStrategies.PreferConsistent(), ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams));
-
-        stream.mapToPair(
-                new PairFunction<ConsumerRecord<String, String>, String, String>() {
-                    public Tuple2<String, String> call(ConsumerRecord<String, String> record) {
-                        return new Tuple2<String, String>(record.key(), record.value());
-                    }
-                });
-
-        stream.map(ConsumerRecord::value).print();
+        KafkaUtils.createDirectStream(
+                        jsc,
+                        LocationStrategies.PreferConsistent(),
+                        ConsumerStrategies.<String, String>Subscribe(List.of(KafkaConstants.TOPIC_NAME), kafkaParams)
+                )
+                .map(ConsumerRecord::value)
+                .print();
 
         jsc.start();
-        jsc.awaitTermination();
 
+        try {
+            jsc.awaitTermination();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
