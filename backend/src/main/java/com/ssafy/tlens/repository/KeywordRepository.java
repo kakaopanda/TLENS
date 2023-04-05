19d282b1039ce46a5b6b0cd5da3712b54f32f8b8
박시형 <qkrtlgud3286@naver.com>
2023-04-05 17:58:50 +0900

    fix: keyword 기능 코드 수정

--- backend/src/main/java/com/ssafy/tlens/repository/KeywordRepository.java
+++ backend/src/main/java/com/ssafy/tlens/repository/KeywordRepository.java
@@ -1,12 +1,14 @@
 package com.ssafy.tlens.repository;
 
 import com.ssafy.tlens.entity.rdbms.Keyword;
-import com.ssafy.tlens.entity.rdbms.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
-    Optional<Keyword> findByUserAndName(User user, String name);
+    @Query("SELECT k FROM Keyword k WHERE k.user.userCode = :userId AND k.name = :name")
+    Optional<Keyword> findByUserAndName(@Param("userId") Long userId, @Param("name") String name);
 }
